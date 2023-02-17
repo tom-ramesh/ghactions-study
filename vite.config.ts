578035a9145780ebf3f746c11eb9b5deb1c5e601
Tom R <tom.r@experionglobal.local>
2023-02-17 13:37:28 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/devops-study/",
+  base: "/ghactions-study/",
   // root: "src",
   build: {
     rollupOptions: { output: { dir: "build" } },
