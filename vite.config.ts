3514135dd954ad3b8c3ba2774718da9518252964
Tom R <tom.r@experionglobal.local>
2023-02-17 13:33:33 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/devops-study/",
   // root: "src",
   build: {
     rollupOptions: { output: { dir: "build" } },
